e9dc5db9f573fa8fbec3d3231e89f1df0702307b
Vinesh Reddy 7222 <vineshreddy7222@gmail.com>
2024-02-20 22:43:09 +0530

    n

--- node.js
+++ node.js
@@ -11,5 +11,5 @@ app.post('/register', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(Server is running on port ${port});
+    console.log(`Server is running on port ${port}`);
 });
