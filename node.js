982695e9dd658d83531d498c7d868804d170dcb8
Vinesh Reddy 7222 <vineshreddy7222@gmail.com>
2024-02-20 21:59:23 +0530

    project completed

---
+++ node.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/register', (req, res) => {
+    
+    res.status(200).json({ message: 'Registration successful' });
+});
+
+app.listen(port, () => {
+    console.log(Server is running on port ${port});
+});
---
+++ script.js
@@ -0,0 +1,9 @@
+function registerPatient() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    
+
+    alert('Patient Registered:\nName: ${name}\nEmail: ${email}');
+}
